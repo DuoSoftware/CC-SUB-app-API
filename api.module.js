212e45ee61bce468f62e5f462e6969702e472bbf
Kasun <kasun.w@duosoftware.com>
2018-01-29 16:29:54 +0530

    First commit

---
+++ api.module.js
@@ -0,0 +1,62 @@
+////////////////////////////////
+// App : api
+// Owner  : Ishara Gunathilaka
+// Last changed date : 2017/11/23
+// Version : 6.1.0.10
+// Modified By : Kasun
+/////////////////////////////////
+
+
+(function ()
+{
+	'use strict';
+
+	angular
+		.module('app.api', [])
+		.config(config);
+
+	/** @ngInject */
+	function config($stateProvider, msNavigationServiceProvider, mesentitlementProvider)
+	{
+
+		$stateProvider
+			.state('app.api', {
+				url    : '/api',
+				views  : {
+					'api@app': {
+						templateUrl: 'app/main/api/api.html',
+						controller : 'ApiController as vm'
+					}
+				},
+				resolve: {
+					security: ['$q','mesentitlement','$timeout','$rootScope','$state','$location', function($q,mesentitlement,$timeout,$rootScope,$state, $location){
+						return $q(function(resolve, reject) {
+
+							$timeout(function() {
+								if (true) {
+									resolve(function () {
+										var entitledStatesReturn = mesentitlement.stateDepResolver('api');
+
+										mesentitlementProvider.setStateCheck("api");
+
+										if(entitledStatesReturn !== true){
+											return $q.reject("unauthorized");
+										}
+									});
+								} else {
+									return $location.path('/guide');
+								}
+							});
+						});
+					}]
+				},
+				bodyClass: 'api'
+			});
+
+		msNavigationServiceProvider.saveItem('api', {
+			title    : 'API',
+			state    : 'app.api',
+			weight   : 5
+		});
+	}
+})();
